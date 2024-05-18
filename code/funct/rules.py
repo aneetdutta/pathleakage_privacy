d55dca26b9a71fe00835f0ce2a1a966b28c96b6c
mihirraj dixit <dixitmn6@gmail.com>
2024-05-18 02:38:55 +0200

    Rule 1 added

--- code/funct/rules.py
+++ code/funct/rules.py
@@ -1,24 +1,60 @@
 from modules.devicemanager import DeviceManager
 from modules.device import Device
 
-def rule_1(line1: list,line2: list, devices: list[Device]):#invoked after checking the condition of time and location            
-    sa_1 = [item[0] for item in line1 if item[1] == 'Bluetooth']
-    sb_1 = [item[0] for item in line1 if item[1] == 'WiFi']
-    sc_1 = [item[0] for item in line1 if item[1] == 'LTE']
 
-    sa_2 = [item[0] for item in line2 if item[1] == 'Bluetooth']
-    sb_2 = [item[0] for item in line2 if item[1] == 'WiFi']
-    sc_2 = [item[0] for item in line2 if item[1] == 'LTE']
+{
+    "Bluetooth": [
+        "7RBQFXP9V7XY"
+    ],
+    "WiFi": [
+        "GC7I7ILS676L"
+    ],
+    "LTE": [
+        "LTEDevice70"
+    ]
+}
+
+def old_id_checker(old_id_t0, new_line_tn1, type_):
+    '''To check if old identifier exists anywhere in the tn+1 mapping, if yes, then no linkage, else link them'''
+    for item_tn1 in new_line_tn1:
+        if old_id_t0 in item_tn1[type_]:
+            return True
+    return False
+
+def rule_1(line1: list, line2: list, tn1_list:list, devices: list[Device]):#invoked after checking the condition of time and location  
+    mapping = None 
+    sa_1, sb_1, sc_1 = line1["Bluetooth"] if "Bluetooth" in line1 else [], line1["WiFi"] if "WiFi" in line1 else [], line1["LTE"] if "LTE" in line1 else []
+    sa_2, sb_2, sc_2 = line2["Bluetooth"] if "Bluetooth" in line2 else [], line2["WiFi"] if "WiFi" in line2 else [], line2["LTE"] if "LTE" in line2 else []
+        
+    # Prepare set operations and store in variables
+    set_sa_1, set_sa_2 = set(sa_1), set(sa_2)
+    set_sb_1, set_sb_2 = set(sb_1), set(sb_2)
+    set_sc_1, set_sc_2 = set(sc_1), set(sc_2)
     
-    lengths_1 = (len(sa_1), len(sb_1), len(sc_1))
-    lengths_2 = (len(sa_2), len(sb_2), len(sc_2))
-            
-    if lengths_1 == (1, 0, 0) and lengths_2 == (1, 0, 0):
-        mapping=(sa_1[0],sb_2[0])
-    elif lengths_1 == (0, 1, 0) and lengths_2 == (0, 1, 0):
-        mapping=(sb_1[0],sb_2[0])
-    elif lengths_1 == (0, 0, 1) and lengths_2 == (0, 0, 1):
-        mapping=(sc_1[0],sc_2[0])
+    l1=set_sa_1.symmetric_difference(set_sa_2)
+    l2=set_sb_1.symmetric_difference(set_sb_2)    
+    l3=set_sc_1.symmetric_difference(set_sc_2)
+
+    len_sa_1, len_sb_1, len_sc_1 = len(sa_1), len(sb_1), len(sc_1)
+    len_sa_2, len_sb_2, len_sc_2 = len(sa_2), len(sb_2), len(sc_2)
+    
+    len_l1, len_l2, len_l3 = len(l1), len(l2), len(l3)    
+
+    if (len_sb_1, len_sc_1, len_sb_2, len_sc_2) == (0, 0, 0, 0): #if rest other protocols are null,
+        if len_sa_1 == len_sa_2:
+            if len_l1 == 1 and set_sa_1 != set_sa_2: #check if intersection of bluetooth is 1
+                print(set_sa_1, set_sa_2, "Bluetooth")
+                mapping=l1 if old_id_checker(line1["Bluetooth"][0], tn1_list, 'Bluetooth') else None
+    elif (len_sa_1, len_sc_1, len_sa_2, len_sc_2) == (0, 0, 0, 0):
+        if len_sb_1 == len_sb_2:
+            if len_l2 == 1 and set_sb_1 != set_sb_2:
+                print(set_sb_1, set_sb_2, "wifi")
+                mapping=l2 if old_id_checker(line1["WiFi"][0], tn1_list, 'WiFi') else None
+    elif (len_sa_1, len_sb_1, len_sa_2, len_sb_2) == (0, 0, 0, 0):
+        if len_sc_1 == len_sc_2:
+            if len_l3 == 1 and set_sc_1!=set_sc_2:
+                print(set_sc_1, set_sc_2, "lte")
+                mapping=l3 if old_id_checker(line1["LTE"][0], tn1_list, 'LTE') else None
     else:
         mapping=None
 
@@ -29,38 +65,32 @@ def rule_1(line1: list,line2: list, devices: list[Device]):#invoked after checki
     return mapping, devices
 
 def rule_2(manager: DeviceManager, line1, devices: list[Device]):
-    sa_1 = [item[1] for item in line1 if item[0] == 'Bluetooth']
-    sb_1 = [item[1] for item in line1 if item[0] == 'WiFi']
-    sc_1 = [item[1] for item in line1 if item[0] == 'LTE']
+    sa_1, sb_1, sc_1 = line1["Bluetooth"] if "Bluetooth" in line1 else [], line1["WiFi"] if "WiFi" in line1 else [], line1["LTE"] if "LTE" in line1 else []
     d, d1, d2 = len(sa_1), len(sb_1), len(sc_1)
     set_sa_1, set_sb_1, set_sc_1  = set(sa_1), set(sb_1), set(sc_1)
-    
+
     if d==1 and d1==1 and d2!=1:
         mapping=(set_sa_1,set_sb_1)
-        manager.create_device(set_sa_1.pop(),set_sb_1.pop(),None)
+        manager.create_device(next(iter(set_sa_1)),next(iter(set_sb_1)),None)
     elif d!=1 and d1==1 and d2==1:
         mapping=(set_sb_1,set_sc_1)
-        manager.create_device(None,set_sb_1.pop(),set_sc_1.pop())
+        manager.create_device(None,next(iter(set_sb_1)),next(iter(set_sc_1)))
     elif d==1 and d1==1 and d2==1:
         mapping=(set_sa_1,set_sb_1,set_sc_1)
-        manager.create_device(set_sa_1.pop(),set_sb_1.pop(),set_sc_1.pop())
+        manager.create_device(next(iter(set_sa_1)),next(iter(set_sb_1)),next(iter(set_sc_1)))
     else:
         mapping=None
     
     if mapping is not None:
-        print("by rule 2")
-        print(mapping)
+        # print("by rule 2")
+        # print(mapping)
         devices.append(mapping)
     return devices
    
 def rule_3(manager: DeviceManager, line1, line2, devices: list[Device]):
-    sa_1 = [item[1] for item in line1 if item[0] == 'Bluetooth']
-    sb_1 = [item[1] for item in line1 if item[0] == 'WiFi']
-    sc_1 = [item[1] for item in line1 if item[0] == 'LTE']
-    
-    sa_2 = [item[1] for item in line2 if item[0] == 'Bluetooth']
-    sb_2 = [item[1] for item in line2 if item[0] == 'WiFi']
-    sc_2 = [item[1] for item in line2 if item[0] == 'LTE']
+    sa_1, sb_1, sc_1 = line1["Bluetooth"] if "Bluetooth" in line1 else [], line1["WiFi"] if "WiFi" in line1 else [], line1["LTE"] if "LTE" in line1 else []
+    sa_2, sb_2, sc_2 = line2["Bluetooth"] if "Bluetooth" in line2 else [], line2["WiFi"] if "WiFi" in line2 else [], line2["LTE"] if "LTE" in line2 else []
+
 
     # Prepare set operations and store in variables
     set_sa_1, set_sa_2 = set(sa_1), set(sa_2)
@@ -129,13 +159,8 @@ def rule_3(manager: DeviceManager, line1, line2, devices: list[Device]):
     return mapping, devices
 
 def rule_4(manager: DeviceManager,line1,line2, devices: list[Device]):
-    sa_1 = [item[1] for item in line1 if item[0] == 'Bluetooth']
-    sb_1 = [item[1] for item in line1 if item[0] == 'WiFi']
-    sc_1 = [item[1] for item in line1 if item[0] == 'LTE']
-    
-    sa_2 = [item[1] for item in line2 if item[0] == 'Bluetooth']
-    sb_2 = [item[1] for item in line2 if item[0] == 'WiFi']
-    sc_2 = [item[1] for item in line2 if item[0] == 'LTE']
+    sa_1, sb_1, sc_1 = line1["Bluetooth"] if "Bluetooth" in line1 else [], line1["WiFi"] if "WiFi" in line1 else [], line1["LTE"] if "LTE" in line1 else []
+    sa_2, sb_2, sc_2 = line2["Bluetooth"] if "Bluetooth" in line2 else [], line2["WiFi"] if "WiFi" in line2 else [], line2["LTE"] if "LTE" in line2 else []
     
     # Prepare set operations and store in variables
     set_sa_1, set_sa_2 = set(sa_1), set(sa_2)
@@ -179,104 +204,135 @@ def rule_4(manager: DeviceManager,line1,line2, devices: list[Device]):
     return devices
 
 
-def rule_5(manager: DeviceManager, line1,line2, devices: list[Device]):
-    sa_1 = [item[1] for item in line1 if item[0] == 'Bluetooth']
-    sb_1 = [item[1] for item in line1 if item[0] == 'WiFi']
-    sc_1 = [item[1] for item in line1 if item[0] == 'LTE']
+def elimination(mapping, line1, line2):
+    sa_1, sb_1, sc_1 = line1["Bluetooth"] if "Bluetooth" in line1 else [], line1["WiFi"] if "WiFi" in line1 else [], line1["LTE"] if "LTE" in line1 else []
+    sa_2, sb_2, sc_2 = line2["Bluetooth"] if "Bluetooth" in line2 else [], line2["WiFi"] if "WiFi" in line2 else [], line2["LTE"] if "LTE" in line2 else []
+
+    l = list(set(sa_1) - set(mapping))
+    l1 = list(set(sa_2).intersection(set(l)))
+
+    l2 = list(set(sb_1) - set(mapping))
+    l3 = list(set(sb_2).intersection(set(l2)))
+
+    l4 = list(set(sc_1) - set(mapping))
+    l5 = list(set(sc_2).intersection(set(l4)))
+
+    if len(l1) == 0:
+        if len(l3) == 1 and len(l5) == 1:
+            print(l3[0], l5[0])
+    else:
+        if len(l3) == 1 and len(l5) == 1 and len(l1) == 1:
+            print(l1[0], l3[0], l5[0])
     
-    sa_2 = [item[1] for item in line2 if item[0] == 'Bluetooth']
-    sb_2 = [item[1] for item in line2 if item[0] == 'WiFi']
-    sc_2 = [item[1] for item in line2 if item[0] == 'LTE']  
+    # to do mapping
+    
+    
+    
+    
+    
+    
+    
+    
+# def rule_5(manager: DeviceManager, line1,line2, devices: list[Device]):
+#     sa_1 = [item[1] for item in line1 if item[0] == 'Bluetooth']
+#     sb_1 = [item[1] for item in line1 if item[0] == 'WiFi']
+#     sc_1 = [item[1] for item in line1 if item[0] == 'LTE']
+    
+#     sa_2 = [item[1] for item in line2 if item[0] == 'Bluetooth']
+#     sb_2 = [item[1] for item in line2 if item[0] == 'WiFi']
+#     sc_2 = [item[1] for item in line2 if item[0] == 'LTE']  
       
-    # Prepare set operations and store in variables
-    set_sa_1, set_sa_2 = set(sa_1), set(sa_2)
-    set_sb_1, set_sb_2 = set(sb_1), set(sb_2)
-    set_sc_1, set_sc_2 = set(sc_1), set(sc_2)
+#     # Prepare set operations and store in variables
+#     set_sa_1, set_sa_2 = set(sa_1), set(sa_2)
+#     set_sb_1, set_sb_2 = set(sb_1), set(sb_2)
+#     set_sc_1, set_sc_2 = set(sc_1), set(sc_2)
     
-    d=list(set_sa_2.intersection(set_sa_1))
-    d1=list(set_sb_2.intersection(set_sb_1))
-    d2=list(set_sc_2.intersection(set_sc_1))
+#     d=list(set_sa_2.intersection(set_sa_1))
+#     d1=list(set_sb_2.intersection(set_sb_1))
+#     d2=list(set_sc_2.intersection(set_sc_1))
     
-    len_d = len(d)
-    len_d1 = len(d1)
-    len_d2 = len(d2)
+#     len_d = len(d)
+#     len_d1 = len(d1)
+#     len_d2 = len(d2)
 
-    l1=list(set(sa_2)-set(d))
-    l2=list(set(sb_2)-set(d1))
-    l3=list(set(sc_2)-set(d2))
+#     l1=list(set(sa_2)-set(d))
+#     l2=list(set(sb_2)-set(d1))
+#     l3=list(set(sc_2)-set(d2))
     
-    len_l1, len_l2, len_l3 = len(l1), len(l2), len(l3)
+#     len_l1, len_l2, len_l3 = len(l1), len(l2), len(l3)
     
-    len_sa_1, len_sb_1, len_sc_1 = len(sa_1), len(sb_1), len(sc_1)
-    len_sa_2, len_sb_2, len_sc_2 = len(sa_2), len(sb_2), len(sc_2)
+#     len_sa_1, len_sb_1, len_sc_1 = len(sa_1), len(sb_1), len(sc_1)
+#     len_sa_2, len_sb_2, len_sc_2 = len(sa_2), len(sb_2), len(sc_2)
     
-    if len_l1==0:
-        if len_l2==1 and len_l3==1 and len_d1==len_sb_2 and len_d2==len_sc_2:
-            mapping=((l2[0],l3[0]))
-            manager.create_device(None,l2[0],l3[0])
-        else:
-            mapping=None
-    else:
-        if len_l2==1  and len_l3==1 and len_l1==1 and len_d==len_sc_2 and len_d1==len_sb_2 and len_d2==len_sc_2:
-            mapping=((l1[0],l2[0],l3[0]))
-            manager.create_device(l1[0],l2[0],l3[0])
-        else:
-            mapping=None
+#     if len_l1==0:
+#         if len_l2==1 and len_l3==1 and len_d1==len_sb_2 and len_d2==len_sc_2:
+#             mapping=((l2[0],l3[0]))
+#             manager.create_device(None,l2[0],l3[0])
+#         else:
+#             mapping=None
+#     else:
+#         if len_l2==1  and len_l3==1 and len_l1==1 and len_d==len_sc_2 and len_d1==len_sb_2 and len_d2==len_sc_2:
+#             mapping=((l1[0],l2[0],l3[0]))
+#             manager.create_device(l1[0],l2[0],l3[0])
+#         else:
+#             mapping=None
 
-    if mapping is not None:
-        print("by rule 5")  
-        print(mapping)
-        devices.append(mapping)
+#     if mapping is not None:
+#         print("by rule 5")  
+#         print(mapping)
+#         devices.append(mapping)
     
-    return devices    
+#     return devices    
   
-def rule_6(manager: DeviceManager, line1,line2, devices: list[Device]):
-    sa_1 = [item[1] for item in line1 if item[0] == 'Bluetooth']
-    sb_1 = [item[1] for item in line1 if item[0] == 'WiFi']
-    sc_1 = [item[1] for item in line1 if item[0] == 'LTE']
+# def rule_6(manager: DeviceManager, line1,line2, devices: list[Device]):
+#     sa_1 = [item[1] for item in line1 if item[0] == 'Bluetooth']
+#     sb_1 = [item[1] for item in line1 if item[0] == 'WiFi']
+#     sc_1 = [item[1] for item in line1 if item[0] == 'LTE']
     
-    sa_2 = [item[1] for item in line2 if item[0] == 'Bluetooth']
-    sb_2 = [item[1] for item in line2 if item[0] == 'WiFi']
-    sc_2 = [item[1] for item in line2 if item[0] == 'LTE']    
+#     sa_2 = [item[1] for item in line2 if item[0] == 'Bluetooth']
+#     sb_2 = [item[1] for item in line2 if item[0] == 'WiFi']
+#     sc_2 = [item[1] for item in line2 if item[0] == 'LTE']    
 
-    # Prepare set operations and store in variables
-    set_sa_1, set_sa_2 = set(sa_1), set(sa_2)
-    set_sb_1, set_sb_2 = set(sb_1), set(sb_2)
-    set_sc_1, set_sc_2 = set(sc_1), set(sc_2)
+#     # Prepare set operations and store in variables
+#     set_sa_1, set_sa_2 = set(sa_1), set(sa_2)
+#     set_sb_1, set_sb_2 = set(sb_1), set(sb_2)
+#     set_sc_1, set_sc_2 = set(sc_1), set(sc_2)
     
-    d=list(set_sa_2.intersection(set_sa_1))
-    d1=list(set_sb_2.intersection(set_sb_1))
-    d2=list(set_sc_2.intersection(set_sc_1))
+#     d=list(set_sa_2.intersection(set_sa_1))
+#     d1=list(set_sb_2.intersection(set_sb_1))
+#     d2=list(set_sc_2.intersection(set_sc_1))
     
-    len_d = len(d)
-    len_d1 = len(d1)
-    len_d2 = len(d2)
+#     len_d = len(d)
+#     len_d1 = len(d1)
+#     len_d2 = len(d2)
 
-    l1=list(set(sa_1)-set(d))
-    l2=list(set(sb_1)-set(d1))
-    l3=list(set(sc_1)-set(d2))
+#     l1=list(set(sa_1)-set(d))
+#     l2=list(set(sb_1)-set(d1))
+#     l3=list(set(sc_1)-set(d2))
     
-    len_sa_1, len_sb_1, len_sc_1 = len(sa_1), len(sb_1), len(sc_1)
-    len_sa_2, len_sb_2, len_sc_2 = len(sa_2), len(sb_2), len(sc_2)
+#     len_sa_1, len_sb_1, len_sc_1 = len(sa_1), len(sb_1), len(sc_1)
+#     len_sa_2, len_sb_2, len_sc_2 = len(sa_2), len(sb_2), len(sc_2)
     
-    len_l1, len_l2, len_l3 = len(l1), len(l2), len(l3)
+#     len_l1, len_l2, len_l3 = len(l1), len(l2), len(l3)
 
-    if len_l1==0:
-        if len_l2==1 and len_l3==1 and len_d1==len_sb_1 and len_d2==len_sc_1:
-            mapping=[(l2[0],'WiFi'),(l3[0],'LTE')]
-            manager.create_device(None,l2[0],l3[0])
-        else:
-            mapping=None
-    else:
-        if len_l2==1  and len_l3==1 and len_l1==1 and len_d==len_sc_1 and len_d1==len_sb_1 and len_d2==len_sc_1:
-            mapping=[(l1[0],'Bluetooth'),(l2[0],'WiFi'),(l3[0],'LTE')]
-            manager.create_device(l1[0],l2[0],l3[0])
-        else:
-            mapping=None
+#     if len_l1==0:
+#         if len_l2==1 and len_l3==1 and len_d1==len_sb_1 and len_d2==len_sc_1:
+#             mapping=[(l2[0],'WiFi'),(l3[0],'LTE')]
+#             manager.create_device(None,l2[0],l3[0])
+#         else:
+#             mapping=None
+#     else:
+#         if len_l2==1  and len_l3==1 and len_l1==1 and len_d==len_sc_1 and len_d1==len_sb_1 and len_d2==len_sc_1:
+#             mapping=[(l1[0],'Bluetooth'),(l2[0],'WiFi'),(l3[0],'LTE')]
+#             manager.create_device(l1[0],l2[0],l3[0])
+#         else:
+#             mapping=None
     
-    if mapping is not None: 
-        print("by rule 6")   
-        print(mapping)
-        devices.append(mapping)
+#     if mapping is not None: 
+#         print("by rule 6")   
+#         print(mapping)
+#         devices.append(mapping)
 
-    return devices
+#     return devices
+
+
--- code/tracking.py
+++ code/tracking.py
@@ -1,80 +1,172 @@
 import pickle
 import ijson, json
-import time
+import time, sys
 # import pyximport; pyximport.install()
 import numpy as np
 from modules.device import Device
 from modules.devicemanager import DeviceManager
-from funct.fn import group_lines_by_distance, extract_lines_with_same_time, D_getter, extract_json
+from funct.fn import *
 
-from funct.rules import rule_1, rule_2, rule_3, rule_4, rule_5, rule_6
+from funct.rules import *
 
 # Opening JSON file
 
-# with open("20240506150753_sniffed_data.json") as f:
-#     data = json.load(f)
-
-# with open("20240506150753_sniffed_data.json", "rb") as f:
-#     data = ijson.items(f, "item")
-#     data_array = pd.DataFrame(data) 
+# now = time.time()
+# data = extract_json("20240506150753_sniffed_data.json")
     
-now = time.time()
-data = extract_json("20240506150753_sniffed_data.json")
-    
-print(data)
-print("data loaded", time.time() - now)
+# print(data)
+# print("data loaded", time.time() - now)
+# sys.exit()
 
 mapped_devices = dict()
 linked_ids = dict()
 
-now = time.time()
+# now = time.time()
+# D = D_getter(data)
+# # Open the file in write mode
+# with open("processed_dict.json", "w") as json_file:
+#     # Serialize the dictionary to JSON and write it to the file
+#     json.dump(D, json_file)
 
-D = D_getter(data)
+data = extract_orjson("processed_dict.json")
 
+# D = json_read_process("processed_dict.json")
+
+# print(data)
+# sys.exit()
 print("Processing data completed")
 
 manager = DeviceManager()
 manager.linked_ids = linked_ids
 devices = []
-for target_time in range(0, 7199):
-    l = D.get(target_time, [])
-    new_target = target_time + 1
-    for j in range(new_target, new_target + 1):
-        if j in D:
-            l1 = D[j]
-        else:
-            j = new_target + 1
-            continue
 
-        for item in l:
-            device = rule_2(manager, item, devices)
-            for item1 in l1:
-                mapping, devices = rule_3(manager, item, item1, devices)
-                if mapping is not None:
-                    linked_ids[mapping[0].pop()] = mapping[1].pop()
-                    manager.linked_ids = linked_ids
-                mapping, devices = rule_1(item, item1, devices)
+
+'''Contains array of tn, tn+1 of timestep queue'''
+consecutive_tq = deque(maxlen=2)
+'''Contains list of tn-1 to 0  (past values for elimination)'''
+past_timestep_arr = []
+
+
+'''Fetches timestep and data for that timestep in this loop'''
+for timestep, data in data.items():
+    # print(data)
+    consecutive_tq.append(data)
+    # break
+    for item_tn_num, item_tn in enumerate(consecutive_tq[0]):
+        '''Create intermapping based on rule 2'''
+        device = rule_2(manager, item_tn, devices)
+        '''Add elimination step'''
+
+        if len(consecutive_tq) == 2:
+            for item_tn1_num, item_tn1 in enumerate(consecutive_tq[1]):
+                # mapping, devices = rule_3(manager, item_tn, item_tn1, devices)
                 
-                if mapping is not None:
-                    linked_ids[mapping[0].pop()] = mapping[1].pop()
-                    manager.linked_ids = linked_ids
-                devices = rule_4(manager, item, item1, devices)
-                devices = rule_5(manager, item, item1, devices)
-                devices = rule_6(manager, item, item1, devices)
+                # if mapping is not None:
+                #     linked_ids[mapping[0].pop()] = mapping[1].pop()
+                #     manager.linked_ids = linked_ids
+                    
+                '''Create intra mapping based on rule 1'''
+                mapping, devices = rule_1(item_tn, item_tn1, consecutive_tq[1], devices)
 
-print("done", time.time() - now)
-device: Device
-for device in manager.device_list:
-    print(device.bluetooth_id, device.wifi_id, device.lte_id)
+                # if mapping is not None:
+                #     linked_ids[mapping[0].pop()] = mapping[1].pop()
+                #     manager.linked_ids = linked_ids
+                
+            
+    #     print(timestep)
+    # # TO DO
+    # break
+    
 
-tracking = []
-# Define file path
-pickle_file = "manager.pkl"
-with open(pickle_file, "wb") as file:
-    pickle.dump(manager, file)
+# for target_time in range(0, 7199):
+#     l = D.get(target_time, [])
+#     new_target = target_time + 1
+#     for j in range(new_target, new_target + 1):
+#         if j in D:
+#             l1 = D[j]
+#         else:
+#             j = new_target + 1
+#             continue
 
-file_path = "linked_ids.json"
-# Open the file in write mode
-with open(file_path, "w") as json_file:
-    # Serialize the dictionary to JSON and write it to the file
-    json.dump(linked_ids, json_file)
+#         for item in l:
+#             device = rule_2(manager, item, devices)
+#             for item1 in l1:
+#                 mapping, devices = rule_3(manager, item, item1, devices)
+#                 if mapping is not None:
+#                     linked_ids[mapping[0].pop()] = mapping[1].pop()
+#                     manager.linked_ids = linked_ids
+#                 mapping, devices = rule_1(item, item1, devices)
+                
+#                 if mapping is not None:
+#                     linked_ids[mapping[0].pop()] = mapping[1].pop()
+#                     manager.linked_ids = linked_ids
+#                 devices = rule_4(manager, item, item1, devices)
+#                 devices = rule_5(manager, item, item1, devices)
+#                 devices = rule_6(manager, item, item1, devices)
+    
+'''
+
+Rule 1 needs to be modified in such a way that it checks if the old identifier is still existing?
+Needs to compare within t and t+1
+In rule 3 the same check needs to be performed when the changed identifier belongs to a protocol having higher communication range than the protocol having a fixed set of identifiers.
+Needs to compare within t and t+1
+Rule 4 is the same , just check from time t to (t+n).
+Elimination rules must be implemented.
+Found mapping at time t_m
+Check elimination rules by removing the mapped identities from the remaining set.
+Run it from time t_0 to the time till which this pair exists.
+Note: Don’t save the mapping found in rule 3 or rule 1 when applying elimination.
+
+'''
+    
+
+'''
+Mapping algorithm
+
+Create mapping table
+
+while loop (
+    Check if mapping table already exists:
+    Delete mappings from all the timesteps - (Elimination procedure)
+    Run the while loop again
+    )
+    
+    For loop: Check through all timesteps from t0 to tn
+
+        *apply rule 1 mapping*
+        check timestep tx and tx+1,
+            Here check if old modifier of tx exists in tx+1             
+
+        *apply rule 3 mapping*
+        check if other protocols sets are same: 
+            if larger range:
+                apply rule 1 checks
+                
+                
+        
+        *Apply rule 4 mapping*
+
+
+'''
+
+
+
+
+
+
+# print("done", time.time() - now)
+# device: Device
+# for device in manager.device_list:
+#     print(device.bluetooth_id, device.wifi_id, device.lte_id)
+
+# tracking = []
+# # Define file path
+# pickle_file = "manager.pkl"
+# with open(pickle_file, "wb") as file:
+#     pickle.dump(manager, file)
+
+# file_path = "linked_ids.json"
+# # Open the file in write mode
+# with open(file_path, "w") as json_file:
+#     # Serialize the dictionary to JSON and write it to the file
+#     json.dump(linked_ids, json_file)
